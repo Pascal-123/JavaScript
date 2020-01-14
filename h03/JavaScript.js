9efbde0bd253597f0d4df2bdb5ab07d6c3a857ce
Pascal-123 <2114466@talnet.nl>
2020-01-14 09:52:59 +0100

    h03 af

---
+++ h03/JavaScript.js
@@ -0,0 +1,21 @@
+// text
+document.getElementById("shuffle").innerHTML = "MonkeyShuffle";
+document.getElementById("shuffle").style.textAlign = "center";
+// images
+document.getElementById("div1").style.textAlign = "center";
+document.getElementById("dier1").src = "img/dier1.jpg";
+document.getElementById("dier2").src = "img/dier2.jpg";
+document.getElementById("dier3").src = "img/dier3.jpg";
+document.getElementById("dier4").src = "img/dier4.jpg";
+document.getElementById("dier5").src = "img/dier5.jpg";
+document.getElementById("dier6").src = "img/dier6.jpg";
+document.getElementById("dier7").src = "img/dier7.jpg";
+document.getElementById("dier8").src = "img/dier8.jpg";
+document.getElementById("dier9").src = "img/dier9.jpg";
+
+
+
+
+
+
+
