a0bdc9a645841a1b94f9073294ce18ad69ae2a33
Pascal-123 <2114466@talnet.nl>
2020-01-14 13:28:53 +0100

    h03 af

--- h03/JavaScript.js
+++ h03/JavaScript.js
@@ -1,8 +1,14 @@
-// text
+// h1
 document.getElementById("shuffle").innerHTML = "MonkeyShuffle";
 document.getElementById("shuffle").style.textAlign = "center";
-// images
-document.getElementById("div1").style.textAlign = "center";
+document.getElementById("shuffle").style.fontFamily = "Copse, serif";
+// div
+document.getElementById("div1").style.width = "900px";
+document.getElementById("div1").style.height = "900px";
+document.getElementById("div1").style.margin = "auto";
+document.getElementById("div1").style.fontSize = "0px";
+document.getElementById("div1").style.border = "solid 1px black";
+// img
 document.getElementById("dier1").src = "img/dier1.jpg";
 document.getElementById("dier2").src = "img/dier2.jpg";
 document.getElementById("dier3").src = "img/dier3.jpg";
@@ -12,6 +18,25 @@ document.getElementById("dier6").src = "img/dier6.jpg";
 document.getElementById("dier7").src = "img/dier7.jpg";
 document.getElementById("dier8").src = "img/dier8.jpg";
 document.getElementById("dier9").src = "img/dier9.jpg";
+// shuffle
+var img = document.getElementsByTagName("img");
+var random = 0;
+var randomNumbers = [];
+while (randomNumbers.length < 9) {
+    random = Math.floor(Math.random() * 9) + 1;
+    if(randomNumbers.lastIndexOf(random) === -1) {
+        randomNumbers.push(random);
+    }
+}
+random = 0;
+for (var img1 in img) {
+    img[img1].src = "img/dier" + randomNumbers[random] + ".jpg";
+    random++;
+}
+
+
+
+
 
 
 
