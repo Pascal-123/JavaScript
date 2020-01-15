cfa58ea16928068cd1e5cdcfc22a0f47e6c4df71
Pascal-123 <2114466@talnet.nl>
2020-01-15 10:40:08 +0100

    4.3 echt af

--- h04/Opdracht 4.3/JavaScript.js
+++ h04/Opdracht 4.3/JavaScript.js
@@ -3,7 +3,7 @@ var teller1 = 0;
 var slideholder1 = document.getElementById("slideholder1");
 slideholder1.style.margin = "auto";
 slideholder1.style.backgroundColor = "#808080";
-slideholder1.style.backgroundImage = "url('img/head1.jpg')";
+slideholder1.style.backgroundImage = "url('img/head3.jpg')";
 slideholder1.addEventListener("click", function () {
     slideholder1.style.backgroundImage = "url('img/head" + getHead() + ".jpg')";
 });
@@ -23,7 +23,7 @@ var teller2 = 0;
 var slideholder2 = document.getElementById("slideholder2");
 slideholder2.style.margin = "auto";
 slideholder2.style.backgroundColor = "#808080";
-slideholder2.style.backgroundImage = "url('img/body1.jpg')";
+slideholder2.style.backgroundImage = "url('img/body3.jpg')";
 slideholder2.addEventListener("click", function () {
     slideholder2.style.backgroundImage = "url('img/body" + getBody() + ".jpg')";
 });
@@ -43,7 +43,7 @@ var teller3 = 0;
 var slideholder3 = document.getElementById("slideholder3");
 slideholder3.style.margin = "auto";
 slideholder3.style.backgroundColor = "#808080";
-slideholder3.style.backgroundImage = "url('img/legs1.jpg')";
+slideholder3.style.backgroundImage = "url('img/legs2.jpg')";
 slideholder3.addEventListener("click", function () {
     slideholder3.style.backgroundImage = "url('img/legs" + getLegs() + ".jpg')";
 });
