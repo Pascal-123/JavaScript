12ea7fb7192bdf8e6b6a9543934643f2753bf214
Pascal-123 <2114466@talnet.nl>
2020-01-15 10:43:15 +0100

    4.3 echt echt af

--- h04/Opdracht 4.3/JavaScript.js
+++ h04/Opdracht 4.3/JavaScript.js
@@ -2,7 +2,7 @@ var img1 = [1,2,3];
 var teller1 = 0;
 var slideholder1 = document.getElementById("slideholder1");
 slideholder1.style.margin = "auto";
-slideholder1.style.backgroundColor = "#808080";
+slideholder1.style.backgroundColor = "#ffffff";
 slideholder1.style.backgroundImage = "url('img/head3.jpg')";
 slideholder1.addEventListener("click", function () {
     slideholder1.style.backgroundImage = "url('img/head" + getHead() + ".jpg')";
@@ -22,7 +22,7 @@ var img2 = [1,2,3];
 var teller2 = 0;
 var slideholder2 = document.getElementById("slideholder2");
 slideholder2.style.margin = "auto";
-slideholder2.style.backgroundColor = "#808080";
+slideholder2.style.backgroundColor = "#ffffff";
 slideholder2.style.backgroundImage = "url('img/body3.jpg')";
 slideholder2.addEventListener("click", function () {
     slideholder2.style.backgroundImage = "url('img/body" + getBody() + ".jpg')";
@@ -42,7 +42,7 @@ var img3 = [1,2,3];
 var teller3 = 0;
 var slideholder3 = document.getElementById("slideholder3");
 slideholder3.style.margin = "auto";
-slideholder3.style.backgroundColor = "#808080";
+slideholder3.style.backgroundColor = "#ffffff";
 slideholder3.style.backgroundImage = "url('img/legs2.jpg')";
 slideholder3.addEventListener("click", function () {
     slideholder3.style.backgroundImage = "url('img/legs" + getLegs() + ".jpg')";
