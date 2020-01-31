50ecba2e7f726bb65200446270e053f75393ef86
Pascal <pascaldewit@outlook.com>
2020-01-31 21:26:50 +0100

    ..

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-var images = document.getElementsByTagName("img");
-var random = 0;
-var randomNumbers = [];
+const images = document.getElementsByTagName("img");
+let random = 0;
+const randomNumbers = [];
 while (randomNumbers.length < 9 ) {
     random = Math.floor(Math.random() * 9) + 1;
     if (randomNumbers.lastIndexOf(random) === -1) {
@@ -9,7 +9,7 @@ while (randomNumbers.length < 9 ) {
 }
 
 random = 0;
-for (var image in images) {
+for (const image in images) {
     images[image].src = "img/card" + randomNumbers[random] + ".svg";
     random++;
 }
