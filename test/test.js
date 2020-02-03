43ceaf0fc01fd868c77862b9be74e5c46efb4f1a
Pascal-123 <2114466@talnet.nl>
2020-02-03 08:58:56 +0100

    ..

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 const images = document.getElementsByTagName("img");
 let random = 0;
 const randomNumbers = [];
-while (randomNumbers.length < 9 ) {
-    random = Math.floor(Math.random() * 9) + 1;
+while (randomNumbers.length < 18 ) {
+    random = Math.floor(Math.random() * 18) + 1;
     if (randomNumbers.lastIndexOf(random) === -1) {
         randomNumbers.push(random);
     }
