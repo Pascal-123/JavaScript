4f305fe772a07b641d0780793857d6e38748fa1a
Pascal-123 <2114466@talnet.nl>
2020-02-05 14:27:58 +0100

    memory game 95% af

--- h06/JavaScript.js
+++ h06/JavaScript.js
@@ -1,41 +1,61 @@
-// create an array of all elements using the ".cards" css selector
+// maakt een lijst van alle elementen die de ".cards" css selector gebruiken
 let cards = document.querySelectorAll(".cards");
 
-// toggle class "flip" when function is called for
+cardsShuffle();
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+// deze function wordt opgeroepen zodra de "addEventListener" wordt geactiveerd
 function flipCard() {
-    this.classList.toggle('flip');
-}
+    // "this" verwijst naar de card waar op geklikt wordt een voegt daar vervolgens een class een toe
+    this.classList.add('flip');
+    // hasFlippedCard is standaard al false dus deze if statement wort uitgevoerd
+    if (hasFlippedCard === false) {
+        // hasFlippedCard switched van false naar true en firstCard wordt nu de kaart waarop geklikt is
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // hasFlippedCard switched van true naar false en secondCard wordt nu de kaart waarop geklikt is
+        hasFlippedCard = false;
+        secondCard = this;
 
-// add addEventListener to every item in the "cards" array
-for (let i = 0; i < cards.length; i++) {
-    let card = cards[i];
-    card.addEventListener("click", flipCard)
-}
-
-// shuffle the cards on refresh
-// comment: voeg later een button toe die deze function oproept op de webpage zelf.
-const cardImages = document.querySelectorAll(".front");
-let random = 0;
-const randomNumbers = [];
-while (randomNumbers.length < 18) {
-    random = Math.floor(Math.random() * 18) + 1;
-    if(randomNumbers.lastIndexOf(random) === -1) {
-        randomNumbers.push(random);
+        checkForMatch();
     }
 }
 
-random = 0;
-
-for (const cardImage in cardImages) {
-    cardImages[cardImage].src = "img/card" + randomNumbers[random] + ".svg";
-    random++;
+function checkForMatch() {
+    if (firstCard.dataset.matching === secondCard.dataset.matching) {
+        disableCards();
+    } else {
+        unflipCards();
+    }
 }
 
-// Clicking on 2 matching cards temporarily prevents the user from clicking on other cards and awards them with a point.
+// als de dataset van 2 cards matchen, "removeEventListener" zodat je dat cards niet terug kan "flippen"
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+}
 
-// comment: zet deze if statement in een function en roep de funcion pas op als function "flip" wordt opgeroepen.
-if (cards[0].dataset.framework === cards[1].dataset.framework) {
-    console.log("rfhvjb")
+// als de dataset van 2 cards NIET matchen, "flip" de kaarten terug naar hun originele staat
+function unflipCards() {
+    setTimeout(function () {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+
+}
+
+function cardsShuffle() {
+    cards.forEach(function(card) {
+        card.style.order = Math.floor(Math.random() * 18);
+    })
+}
+// loop door de "cards" array en voeg een "addEventListener" toe
+for (let i = 0; i < cards.length; i++) {
+    let card = cards[i];
+    card.addEventListener("click", flipCard)
 }
 
 
